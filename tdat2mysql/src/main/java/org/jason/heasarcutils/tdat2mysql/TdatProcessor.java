691eba6f8aeb31fcb27284e19a758f5826657664
Jason Ferguson <fergusonjason@gmail.com>
2011-11-28 08:46:04 -0600

    update package name

--- tdat2mysql/src/main/java/org/jason/heasarcutils/tdat2mysql/TdatProcessor.java
+++ tdat2mysql/src/main/java/org/jason/heasarcutils/tdat2mysql/TdatProcessor.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.jason.tdat2mysql;
+package org.jason.heasarcutils.tdat2mysql;
 
 import java.io.*;
 import java.util.*;
