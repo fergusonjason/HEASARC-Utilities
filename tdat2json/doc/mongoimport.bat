685fcfc91c475a034cd64c324c70753d20891efd
Jason Ferguson <fergusonjason@gmail.com>
2011-11-28 13:14:06 -0600

    initial commit

---
+++ tdat2json/doc/mongoimport.bat
@@ -0,0 +1,11 @@
+mongoimport -d astro2 -c catalog bsc.json
+mongoimport -d astro2 -c catalog cns3.json
+mongoimport -d astro2 -c catalog hd.json
+mongoimport -d astro2 -c catalog hipparcos.json
+mongoimport -d astro2 -c catalog messier.json
+mongoimport -d astro2 -c catalog ngc2000.json
+mongoimport -d astro2 -c catalog ppm.json
+mongoimport -d astro2 -c catalog sao.json
+mongoimport -d astro2 -c catalog ugc.json
+
+
