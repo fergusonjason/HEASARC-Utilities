9056d74952d0945cb5cfcb43a8dfdadaa666824d
Jason Ferguson <fergusonjason@gmail.com>
2011-11-28 15:13:19 -0600

    more processing stuff

--- tdat2json/doc/ppm.js
+++ tdat2json/doc/ppm.js
@@ -1,80 +1,58 @@
-Array.prototype.unique = function(b) {
-    var a = [], i, l = this.length;
-    for (i = 0; i < l; i++) {
-        if (a.indexOf(this[i], 0, b) < 0) {
-            a.push(this[i]);
-        }
-    }
-    return a;
-};
-
-db.ppm.update({"_id":{$exists:true}}, {$set:{"xref":[]}}, false, true);
-db.ppm.ensureIndex({"dmname":1});
+db.catalog.ensureIndex({"dmname":1});
 
 // have to reprocess some of the Durchmusterung names because NASA was lazy
-db.ppm.find({dmname:/^(-[0-9]{2})\s+([0-9]*[A-Z]*)$/i}).forEach(function(obj) {
+db.catalog.find({dmname:/^(-[0-9]{2})\s+([0-9]*[A-Z]*)$/i}).forEach(function(obj) {
     var firstThree = new Number(obj.dmname.substring(0, 3));
     if (firstThree == "NaN") {
         return;
     }
+    // according to the description page, anything below -23 degrees is a CD from the Cordoba observatory, otherwise
+    // it's a BD number
     if (firstThree < -23) {
         obj.dmname = "CD " + obj.dmname;
     } else {
         obj.dmname = "BD " + obj.dmname;
     }
-    db.ppm.save(obj);
+    db.catalog.save(obj);
 
 });
 
-db.ppm.find({dmname:/^(\+[0-9]{2})\s+([0-9]*[A-Z]*)$/i}).forEach(function(obj) {
+db.catalog.find({dmname:/^(\+[0-9]{2})\s+([0-9]*[A-Z]*)$/i}).forEach(function(obj) {
     obj.dmname = "BD " + obj.dmname;
-    db.ppm.save(obj);
+    db.catalog.save(obj);
 });
 
-db.ppm.find().forEach(function(obj) {
-    // star catalogs
-    if (obj.cpdname != null) {
-        obj.xref.push(obj.cpdname);
+db.catalog.find({dmname:/-([0-9]{2})([0-9]+[A-Z])$/i}).forEach(function(obj) {
+    var dmArray = obj.dmname.match(/-([0-9]{2})([0-9]+[A-Z])$/i);
+    if (dmArray.length > 0) {
+        var prefix = new Number(dmArray[1]);
+        if (prefix < -23) {
+            obj.dmname = "CD-" + prefix + " " + dmArray[2];
+        } else {
+            if (prefix < 0) {
+                obj.dmname = "BD-" + prefix + " " + dmArray[2];
+            } else {
+                obj.dmname = "BD+" + prefix + " " + dmArray[2];
+            }
+        }
+        db.catalog.save(obj);
     }
+});
 
+db.catalog.find().forEach(function(obj) {
+    // star catalogs
     if (obj.dmname != null) {
         obj.xref.push(obj.dmname);
     }
-    if (obj.gcname != null) {
-        obj.xref.push(obj.gcname);
-    }
-    if (obj.hdname != null) {
-        obj.xref.push(obj.hdname);
-    }
-    if (obj.ppmname != null) {
-        obj.xref.push(obj.ppmname);
-    }
 
-    db.ppm.save(obj);
+    db.catalog.save(obj);
 });
 
 
-
-db.ppm.find().forEach(function(obj) {
-    tempXref = obj.xref.unique();
-    obj.xref = tempXref;
-    db.ppm.save(obj);
-});
-
-db.ppm.update({"bscname":{$exists:true}}, {$unset:{"bscname":1}}, false, true);
-db.ppm.update({"hdname":{$exists:true}}, {$unset:{"hdname":1}}, false, true);
-db.ppm.update({"saoname":{$exists:true}}, {$unset:{"saoname":1}}, false, true);
-db.ppm.update({"ppmname":{$exists:true}}, {$unset:{"ppmname":1}}, false, true);
-db.ppm.update({"hipname":{$exists:true}}, {$unset:{"hipname":1}}, false, true);
-db.ppm.update({"gcname":{$exists:true}}, {$unset:{"gcname":1}}, false, true);
-db.ppm.update({"dmname":{$exists:true}}, {$unset:{"dmname":1}}, false, true);
-db.ppm.update({"gliesename":{$exists:true}}, {$unset:{"gliesename":1}}, false, true);
-db.ppm.update({"cpdname":{$exists:true}}, {$unset:{"cpdname":1}}, false, true);
-db.ppm.update({"messiername":{$exists:true}}, {$unset:{"messiername":1}}, false, true);
-db.ppm.update({"ngc2000name":{$exists:true}}, {$unset:{"ngc2000name":1}}, false, true);
-db.ppm.update({"ugcname":{$exists:true}}, {$unset:{"ugcname":1}}, false, true);
+db.catalog.update({"dmname":{$exists:true}}, {$unset:{"dmname":1}}, false, true);
 
 
-db.ppm.find({dmname:/^(\+[0-9]{2})\s+([0-9]*[A-Z]*)$/i})
+
+
 
 
