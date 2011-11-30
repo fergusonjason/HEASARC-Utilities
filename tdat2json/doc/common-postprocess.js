9789989706205e4bbfe100eecebc6d381fae8127
Jason Ferguson <fergusonjason@gmail.com>
2011-11-30 14:51:57 -0600

    misc work

--- tdat2json/doc/common-postprocess.js
+++ tdat2json/doc/common-postprocess.js
@@ -5,3 +5,21 @@ db.catalog.find().forEach(function(obj) {
     obj.xref = tempXref;
     db.catalog.save(obj);
 });
+
+db.catalog.find({ccdmname:{$exists:1}}).forEach(function(obj) {
+    if (obj.ccdmname !=null) {
+        obj.xref.push(obj.ccdmname);
+    }
+    db.catalog.save(obj);
+});
+
+db.catalog.update({ir_flag:{$exists:1}},{$unset:{ir_flag:1}},false,true);
+db.catalog.update({notes:{$exists:1}},{$unset:{notes:1}},false,true);
+db.catalog.update({id_chart:{$exists:1}},{$unset:{id_chart:1}},false,true);
+db.catalog.update({ads:{$exists:1}},{$unset:{ads:1}},false,true);
+db.catalog.update({ads_comp:{$exists:1}},{$unset:{ads_comp:1}},false,true);
+db.catalog.update({dmnumber:{$exists:1}},{$unset:{dmnumber:1}},false,true);
+
+
+
+
