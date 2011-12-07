ffc28fa6530484930d3fdb8ccd5c452c23597bb9
Jason Ferguson <fergusonjason@gmail.com>
2011-12-07 09:31:40 -0600

    initial commit

---
+++ catalogparser/src/main/java/org/jason/heasarcutils/catalogparser/util/Catalog.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2011 Jason Ferguson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jason.heasarcutils.catalogparser.util;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since 0.1
+ * @author Jason Ferguson
+ */
+public class Catalog implements Serializable {
+
+    private String name;
+    private String url;
+    private Map<String, FieldData> fieldData = new HashMap<String, FieldData>();
+    private Map<String, String> prefixes = new HashMap<String, String>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Map<String, FieldData> getFieldData() {
+        return fieldData;
+    }
+
+    public void setFieldData(Map<String, FieldData> fieldData) {
+        this.fieldData = fieldData;
+    }
+
+    public Map<String, String> getPrefixes() {
+        return prefixes;
+    }
+
+    public void setPrefixes(Map<String, String> prefixes) {
+        this.prefixes = prefixes;
+    }
+}
