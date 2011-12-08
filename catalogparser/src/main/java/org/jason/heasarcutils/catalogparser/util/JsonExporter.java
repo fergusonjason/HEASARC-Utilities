dafafae271cedd0d299c89c2e03fd3977a1dd5d8
Jason Ferguson <fergusonjason@gmail.com>
2011-12-08 14:12:48 -0600

    interim commit

--- catalogparser/src/main/java/org/jason/heasarcutils/catalogparser/util/JsonExporter.java
+++ catalogparser/src/main/java/org/jason/heasarcutils/catalogparser/util/JsonExporter.java
@@ -15,17 +15,131 @@
  */
 package org.jason.heasarcutils.catalogparser.util;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
 /**
  * Class to handle the export to JSON
  *
- * @since 0.1
  * @author Jason Ferguson
+ * @since 0.1
  */
 public class JsonExporter {
 
-    public static void exportToJSON(Catalog catalog) {
+    private Catalog catalog;
+
+    public JsonExporter(Catalog catalog) {
+        this.catalog = catalog;
+    }
+
+    public void exportToJSON() {
 
         System.out.println("Exporting " + catalog.getName());
+        if (catalog.getType().equals("tdat")) {
+            // import tdat file
+            String filename = catalog.getUrl();
+        } else {
+            // import dat file
+            String filename = catalog.getUrl();
+            processDatFile(filename);
+        }
 
+    }
+
+    private void processTdatFile(String filename) {
+
+    }
+
+    private void processDatFile(String filename) {
+
+        try {
+            URL url = new URL(filename);
+            // set up the data input
+            GZIPInputStream gzis = new GZIPInputStream(url.openStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(gzis));
+            String line = reader.readLine();
+
+            // set up the data output
+            BufferedWriter writer = new BufferedWriter(new FileWriter(catalog.getName() + ".json"));
+
+            // create a template so I only have to create a map once
+            Map<String, String> template = new LinkedHashMap<String, String>(catalog.getFieldData().size());
+            for (String fieldName : catalog.getFieldData().keySet()) {
+                template.put(fieldName, null);
+            }
+
+            while (line != null) {
+                Map<String, String> fieldMap = template;
+                for (String key : catalog.getFieldData().keySet()) {
+                    FieldData fd = catalog.getFieldData().get(key);
+                    fieldMap.put(key, line.substring(fd.getStart(), fd.getEnd()));
+                }
+
+                fieldMap = removeNulls(fieldMap);
+
+                writer.write(getJsonLine(fieldMap));
+                line = reader.readLine();
+            }
+
+            writer.close();
+
+            reader.close();
+            gzis.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Map<String, String> removeNulls(Map<String, String> map) {
+
+        Map<String, String> result = new LinkedHashMap<String, String>();
+
+        for (String key: map.keySet()) {
+            if (map.get(key) != null) {
+                result.put(key, map.get(key));
+            }
+        }
+
+        return result;
+    }
+    private String getJsonLine(Map<String, String> data) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        for (String key : data.keySet()) {
+            sb.append(key);
+            sb.append(":");
+            if (isNumber(data.get(key))) {
+                sb.append(data.get(key));
+            } else {
+                sb.append("\"");
+                sb.append(data.get(key));
+                sb.append("\"");
+            }
+            sb.append(",");
+        }
+        sb = new StringBuffer(sb.substring(0, sb.length() - 1)); // stupid trailing comma
+        sb.append("}");
+
+        return sb.toString();
+    }
+
+    private boolean isInteger(String value) {
+        String pattern = "^[0-9]+$";
+        return value.matches(pattern);
+    }
+
+    private boolean isDouble(String value) {
+        String pattern = "^[0-9]+\\.[0-9]*$";
+        return value.matches(pattern);
+    }
+
+    private boolean isNumber(String value) {
+        return (isInteger(value) || isDouble(value));
     }
 }
